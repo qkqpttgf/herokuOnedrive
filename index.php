7b91b4879f1ffef7d0ce589a64f6618aef1c3a32
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-11-29 17:33:19 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,15 +1,44 @@
 <?php
-   class MyDB extends SQLite3
-   {
-      function __construct()
-      {
-         $this->open('test.db');
-      }
-   }
-   $db = new MyDB();
-   if(!$db){
-      echo $db->lastErrorMsg();
-   } else {
-      echo "Opened database successfully\n";
-   }
+//include "pa.php";
+include "phpqrcode.php";
+
+
+// ▒▒ɵ▒▒ļ▒▒▒
+$filename = 'QR-'.date("Ymd-His").'.png';
+// ▒▒▒▒▒L▒▒M▒▒Q▒▒H
+$errorCorrectionLevel = 'L';
+// ▒▒Ĵ▒С▒▒1▒▒10
+$matrixPointSize = 2;
+//▒▒▒▒һ▒▒▒ά▒▒▒ļ▒
+if($_GET["f"]==1){
+QRcode::png($_GET["key"], $filename, $errorCorrectionLevel, $matrixPointSize, 1);
+}
+//▒▒▒▒▒ά▒뵽▒▒▒▒▒
+//QRcode::png($data);
+QRcode::png($_GET["key"], false, $errorCorrectionLevel, $matrixPointSize, 1);
+
+/*
+$value=$_GET["key"];
+$logo = 'favicon.png'; // ▒м▒▒logo
+$QR = "base.png"; // ▒Զ▒▒▒▒▒ɵġ▒▒▒▒▒▒▒▒▒ɾ▒▒
+$last = "last.png"; // ▒▒▒▒▒▒ɵ▒ͼƬ
+$errorCorrectionLevel = 'L';
+$matrixPointSize = 10;
+QRcode::png($value, $QR, $errorCorrectionLevel, $matrixPointSize, 2);
+if($logo !== FALSE){
+    $QR = imagecreatefromstring(file_get_contents($QR));
+    $logo = imagecreatefromstring(file_get_contents($logo));
+    $QR_width = imagesx($QR);
+    $QR_height = imagesy($QR);
+    $logo_width = imagesx($logo);
+    $logo_height = imagesy($logo);
+    $logo_qr_width = $QR_width / 5;
+    $scale = $logo_width / $logo_qr_width;
+    $logo_qr_height = $logo_height / $scale;
+    $from_width = ($QR_width - $logo_qr_width) / 2;
+    imagecopyresampled($QR, $logo, $from_width, $from_width, 0, 0, $logo_qr_width, $logo_qr_height, $logo_width, $logo_height);
+}
+imagepng($QR,$last); // ▒▒▒▒▒▒յ▒▒ļ▒
+echo "<img src=last.png>";
+*/
 ?>
