791ca5de3a5397cd7af000e6fe8eadcb12aefac4
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-11-29 17:57:08 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <?php
 //echo $_SERVER['REMOTE_ADDR'];
+echo '<pre>';
 echo json_encode($_GET, JSON_PRETTY_PRINT) . '
 ';
 echo json_encode($_POST, JSON_PRETTY_PRINT) . '
@@ -8,5 +9,5 @@ echo json_encode($_SERVER, JSON_PRETTY_PRINT) . '
 ';
 echo json_encode($_COOKIE, JSON_PRETTY_PRINT) . '
 ';
-
+echo '</pre>';
 ?>
