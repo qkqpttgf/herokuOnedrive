de0f4ab1eb038e8c693013b50e42ed0254b6e8a5
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-11-30 12:51:16 +0800

    Update index.php

--- index.php
+++ index.php
@@ -54,7 +54,7 @@ if ($context['request_id']=='') {
     foreach ($getstrarr as $getvalues) {
         $pos = strpos($getvalues,"=");
 		//echo $pos;
-		if ($pos>0) {
+		if ($getvalues!=''&&$pos>0) {
 			$getarry[urldecode(substr($getvalues,0,$pos))] = urldecode(substr($getvalues,$pos+1));
 		} else $getarry[urldecode($getvalues)] = true;
     }
@@ -124,7 +124,7 @@ function main_handler($event, $context)
     $config['is_imgup_path'] = 0;
     if (path_format('/'.path_format(urldecode($config['list_path'].path_format($path))).'/')==path_format('/'.path_format($config['imgup_path']).'/')&&$config['imgup_path']!='') $config['is_imgup_path'] = 1;
     $_GET = $event['queryString'];
-	echo '<pre>'.json_encode($_GET, JSON_PRETTY_PRINT).'</pre>';
+	//echo '<pre>'.json_encode($_GET, JSON_PRETTY_PRINT).'</pre>';
     $_SERVER['PHP_SELF'] = path_format($config['base_path'] . $path);
     $_SERVER['REMOTE_ADDR'] = $event['requestContext']['sourceIp'];
     /*$_POSTbody = explode("&",$event['body']);
