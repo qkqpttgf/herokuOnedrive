2a25eb2d340b8d9947e055f3de0a718b8242f985
qkqpttgf <45693631+qkqpttgf@users.noreply.github.com>
2019-12-28 14:39:05 +0800

    Update index.php

--- index.php
+++ index.php
@@ -31,7 +31,7 @@ if ($_SERVER['USER']!='qcloud') {
         'x-requested-with' => '',
     ];
     if (isset($_SERVER['HTTP_X_REQUESTED_WITH'])) $event['headers']['x-requested-with'] = $_SERVER['HTTP_X_REQUESTED_WITH'];
-    if ($_SERVER['REDIRECT_URL']=='') {
+    if (!$_SERVER['REDIRECT_URL']) {
         $_SERVER['REDIRECT_URL']='/';
     } else $_SERVER['REDIRECT_URL']=spurlencode($_SERVER['REDIRECT_URL'], '/');
     $event['path'] = $_SERVER['REDIRECT_URL'];
